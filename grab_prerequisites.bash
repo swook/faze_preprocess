4e76176ecccc127f3fa5c03acfdb7f162ddd612d
Seon-Wook Park <seon.wook@swook.net>
2023-06-06 15:53:45 +0900

    Update broken .h5 file links

--- grab_prerequisites.bash
+++ grab_prerequisites.bash
@@ -4,8 +4,8 @@
 pip3 install --user -r requirements.txt
 
 # Download required additional labels
-wget -N https://ait.ethz.ch/projects/2019/faze/downloads/preprocessing/MPIIFaceGaze_supplementary.h5
-wget -N https://ait.ethz.ch/projects/2019/faze/downloads/preprocessing/GazeCapture_supplementary.h5
+wget -N https://files.ait.ethz.ch/projects/faze/preprocessing/MPIIFaceGaze_supplementary.h5
+wget -N https://files.ait.ethz.ch/projects/faze/preprocessing/GazeCapture_supplementary.h5
 
 # Download eos files for pre-processing for ST-ED
 mkdir -p ./eos/
